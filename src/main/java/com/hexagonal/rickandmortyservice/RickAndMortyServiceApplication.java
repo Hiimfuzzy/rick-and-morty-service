72efa46bab9d7c3deced2ce377dad37ce55e4743
Hiimfuzzy <jccortes06@ucatolica.edu.co>
2024-07-11 16:09:34 -0500

    start project

--- src/main/java/com/hexagonal/rickandmortyservice/RickAndMortyServiceApplication.java
+++ src/main/java/com/hexagonal/rickandmortyservice/RickAndMortyServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RickAndMortyServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RickAndMortyServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RickAndMortyServiceApplication.class, args);
+    }
 
 }
