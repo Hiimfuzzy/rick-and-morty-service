5512216676685de87b1effdc80d50c8f021a494e
Hiimfuzzy <jccortes06@ucatolica.edu.co>
2024-07-11 16:07:56 -0500

    starting project

---
+++ src/test/java/com/hexagonal/rickandmortyservice/RickAndMortyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonal.rickandmortyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
